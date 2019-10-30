9f16d9ab7d8695a971d6953bd4321ede1eb7d714
Rui <rui.chen1996@gmail.com>
2019-10-30 12:37:36 +0100

    Brancher le provider Redux et le store

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Tchat from './component/Tchat';
 
+import { Provider } from 'react-redux'
+import reducers from './reducers/message'
+import { createStore } from 'redux'
+
+//Brancher le provider Redux et le store
+let store = createStore(reducers)
+
+/**acces au store redux */
 function App() {
   return (
     <div className="App">
-      <Tchat />
+      
+      <Provider store={store}>
+         <Tchat />
+      </Provider>
+  
     </div>
   );
 }
-
 export default App;
