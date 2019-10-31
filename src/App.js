e09820c11f8554a824b538dd1d1c3b17328d33cb
Rui <rui.chen1996@gmail.com>
2019-10-31 17:37:13 +0100

    Redux Middleware

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 import Tchat from './component/Tchat';
+import Login from './component/Login';
 
-import { Provider } from 'react-redux'
+//import { Provider } from 'react-redux'
 import reducers from './reducers/message'
 import { createStore } from 'redux'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 //Brancher le provider Redux et le store
 let store = createStore(reducers)
@@ -12,12 +20,15 @@ let store = createStore(reducers)
 /**acces au store redux */
 function App() {
   return (
-    <div className="App">
-      
-      <Provider store={store}>
-         <Tchat />
-      </Provider>
-  
+    <div >
+      <nav>
+        <link to = "/">login</link>
+        <link to = "/tchat">Tchat</link>
+      </nav>
+      <switch>
+        <Route path="/" exact> <Login /></Route>
+        <Route path="/" exact> <Tchat /></Route>
+      </switch>  
     </div>
   );
 }
